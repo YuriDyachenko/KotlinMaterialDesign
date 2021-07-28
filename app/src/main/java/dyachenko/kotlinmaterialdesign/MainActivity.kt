86b0bf72737f1346d892c04423cb4c9c5b6e312b
YuriDyachenko <yuri.dyachenko@outlook.com>
2021-07-28 19:19:41 +0300

    Initial commit

---
+++ app/src/main/java/dyachenko/kotlinmaterialdesign/MainActivity.kt
@@ -0,0 +1,18 @@
+package dyachenko.kotlinmaterialdesign
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dyachenko.kotlinmaterialdesign.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
